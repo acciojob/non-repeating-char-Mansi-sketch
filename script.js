306e8b28a49728cc1ab514759fdb9b77ab88caaf
Mansi-sketch <129894321+Mansi-sketch@users.noreply.github.com>
2025-07-16 22:26:06 +0200

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	let str1 = str.toLowerCase();
+	for (let i = 0; i < str1.length; i++) {
+		let char = str1[i];
+		if(str1.indexOf(char) === str1.lastIndexOf(char)){
+			return str[i];
+		}
+	}
+	return null;	
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
